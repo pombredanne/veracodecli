e0c77b7fb81ed748f48c7c22365e9f1abf35b553
= <=>
2015-09-21 09:40:52 -0700

    tweaked error message, minor change to log functions (not enabled yet)

--- apicore/API_functions.rb
+++ apicore/API_functions.rb
@@ -4,7 +4,7 @@ require 'rest-client'
 
 module VeracodeApiBase
 	def check_environment_login_variables
-		fail 'EnvironmentError: VERACODE_USERNAME or VERACODE_PASSWORD not set.' unless ENV['VERACODE_USERNAME'] != nil && ENV['VERACODE_PASSWORD'] != nil
+		if ENV['VERACODE_USERNAME'].nil? || ENV['VERACODE_PASSWORD'].nil? || ENV['VERACODE_TEAM'].nil? then fail 'EnvironmentError: VERACODE_USERNAME,VERACODE_PASSWORD or VERACODE_TEAM not set.'
 	end
 
 	def veracode_api_request(api_call, api_version: '4.0', **params)
@@ -21,7 +21,7 @@ module VeracodeApiBase
 
 	def write(data, to_file:)
 		data = xml_to_json data
-		f = File.open "../testdata/#{to_file}.json", 'w'
+		f = File.open "../logs/#{to_file}.json", 'a'
 		f.write JSON.pretty_generate data
 		f.close
 	end
@@ -31,7 +31,7 @@ module VeracodeApiScan
 
 	include VeracodeApiBase
 
-	def validate_existance(of:, by:)
+	def validate_existance(of:)
 		puts "Validating records for #{of}"
 		app_list = veracode_api_request 'getapplist.do', include_user_info: 'true'
 		if app_list.include? "#{of}"
@@ -41,6 +41,7 @@ module VeracodeApiScan
 			puts 'Record not found, creating one'
 			create_app_result = veracode_api_request 'createapp.do', app_name: of, description: "Static Scanning profile for #{of}.", business_criticality: 'High', business_unit: 'TELUS Digital', web_application: 'true', teams: "#{ENV['TEAM']}"
 			puts create_app_result
+			#write create_app_result, to_file: "profile_creation_log"
 			app_id = create_app_result.scan(/app_id=\"(.+)\" app_name=\"#{of}\"/)[0][0]
 			puts "Record successfully created, app_id is #{app_id}"
 			return app_id
@@ -48,16 +49,16 @@ module VeracodeApiScan
 	end
 
 	def submit_scan(hostname, archive_path)
-		app_id = validate_existance of: hostname, by: ENV['VERACODE_USERNAME']
-		#NOTE: curl must be used here because of a bug in the Veracode api. Ruby cannot be used while this bug is present.
+		app_id = validate_existance of: hostname
+		#NOTE: curl must be used here because of a bug in the Veracode api. Pure Ruby cannot be used while this bug is present.
 		#NOTE: preferred code: upload_result = veracode_api_request 'uploadfile.do', app_id: app_id, file: "#{archive_path}"
 		upload_result = `curl --url "https://#{ENV['VERACODE_USERNAME']}:#{ENV['VERACODE_PASSWORD']}@analysiscenter.veracode.com/api/4.0/uploadfile.do" -F 'app_id=#{app_id}' -F 'file=@#{archive_path}'`
 		puts upload_result
-		#write upload_result, to_file: "#{app_id}_upload_result"
+		#write upload_result, to_file: "#{app_id}_upload_log"
 		prescan_submission_result = veracode_api_request 'beginprescan.do', app_id: app_id, auto_scan: 'true'
 		puts prescan_submission_result
 		puts "Submit complete for #{app_id}"
-		#write prescan_submission_result, to_file: "#{app_id}_prescan_submission_result"
+		#write prescan_submission_result, to_file: "#{app_id}_prescan_submission_log"
 	end
 end
 
