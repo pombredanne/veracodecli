d3b3556a0eeedd5d54141c9ad1f86f3ea1a2f2e8
= <=>
2015-10-23 15:32:44 -0700

    test changes

--- test/test_veracodecli.rb
+++ test/test_veracodecli.rb
@@ -1,7 +1,20 @@
 require 'test/unit'
-require 'shoulda'
+require 'shoulda/context'
+require_relative '../lib/veracodecli/api'
+include VeracodeApiScan
 
 class TestVeracodecli < Test::Unit::TestCase
+  context 'VeracodeApi' do
 
+    setup do
+      ENV['VERACODE_USERNAME'] = 'foo'
+      ENV['VERACODE_PASSWORD'] = 'bar'
+      ENV['VERACODE_TEAM'] = 'foobar'
+    end
 
+    should 'Return HTTP 401' do
+      assert_equal response.code, veracode_api_request('getapplist.do', '')
+    end
+
+  end
 end
